eac39167c78233234516c08e3d08fd13ba5efce8
= <=>
2024-01-18 01:38:59 +0500

    commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,31 +1,32 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 const links = [
-  { href: "/client", label: "Client" },
-  { href: "/drinks", label: "Drinks" },
-  { href: "/tasks", label: "Tasks" },
-  { href: "/prisma-example", label: "Prisma" },
+  { href: '/client', label: 'client' },
+  { href: '/drinks', label: 'drinks' },
+  { href: '/prisma-example', label: 'prisma' },
+  { href: '/tasks', label: 'tasks' },
 ];
 
 const Navbar = () => {
   return (
-    <nav className="bg-base-300 py-4">
-      <div className="navbar px-8 max-w-6xl mx-auto flex-col sm:flex-row">
-        <Link href="/" className="btn btn-primary">
+    <nav className='bg-base-300 py-4'>
+      <div className='navbar px-8 max-w-6xl mx-auto flex-col sm:flex-row'>
+        <Link href='/' className='btn btn-primary'>
           Next.js
         </Link>
-        <ul className="menu menu-horizontal md:ml-8">
-          {links.map((link, id) => (
-            <li key={id} className="mr-2">
-              <Link href={link.href} className="capitalize">
-                {link.label}
-              </Link>
-            </li>
-          ))}
+        <ul className='menu menu-horizontal md:ml-8'>
+          {links.map((link) => {
+            return (
+              <li key={link.href}>
+                <Link href={link.href} className='capitalize'>
+                  {link.label}
+                </Link>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </nav>
   );
 };
-
 export default Navbar;
